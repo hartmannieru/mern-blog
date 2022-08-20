6cb88f89b3d81cfebc61cb88d127fb4c282cf3c0
Annie <anniehartmann97@gmail.com>
2022-08-20 18:56:51 +0300

    Refactoring and add multer

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import multer from 'multer';
 import mongoose from 'mongoose';
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 import { register, login, getMe } from './controllers/UserController.js';
 import { create, getAll, getOne, remove, update } from './controllers/PostController.js';
+import handleValidationErrors from './utils/handleValidationErrors.js';
 
 mongoose
   .connect('mongodb+srv://admin:wwwwww@cluster0.tzq99.mongodb.net/blog?retryWrites=true&w=majority')
@@ -12,17 +14,36 @@ mongoose
   .catch((err) => console.log('Database Error', err));
 
 const app = express();
-app.use(express.json());
 
-app.post('/auth/login', loginValidation, login);
-app.post('/auth/register', registerValidation, register);
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/login', loginValidation, handleValidationErrors, login);
+app.post('/auth/register', registerValidation, handleValidationErrors, register);
 app.get('/auth/me', checkAuth, getMe);
+
+app.post('/upload', upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
 
 app.get('/posts', getAll);
 app.get('/posts/:id', getOne);
-app.post('/posts', checkAuth, postCreateValidation, create);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, create);
 app.delete('/posts/:id', checkAuth, remove);
-app.patch('/posts/:id', checkAuth, update);
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, update);
 
 app.listen(4444, (err) => {
   if (err) {
