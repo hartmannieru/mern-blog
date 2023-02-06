0ce1dadc5aa32f0eda79f8d9a7f63667cc66e25a
Annie <anniehartmann97@gmail.com>
2023-02-06 16:55:44 +0300

    Upd

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/uploads", express.static("uploads"));
+
 app.post("/auth/login", loginValidation, handleValidationErrors, login);
 app.post(
   "/auth/register",
