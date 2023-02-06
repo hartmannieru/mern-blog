9ba7ed18593601d179af5551645f683ab5486fff
Annie <anniehartmann97@gmail.com>
2023-02-06 17:28:27 +0300

    Upd

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,5 @@
 import PostModel from "../models/Post.js";
-// import cloudinary from "../utils/cloudinary.js";
+import cloudinary from "../utils/cloudinary.js";
 export const getLastTags = async (req, res) => {
   try {
     const posts = await PostModel.find().limit(5).exec();
@@ -99,12 +99,12 @@ export const remove = async (req, res) => {
 };
 
 export const create = async (req, res) => {
-  // const imageName = req.body.imageUrl;
-  // const result = await cloudinary.uploader.upload(`${imageName}`, {
-  //   public_id: `${Date.now()}`,
-  //   resource_type: "auto",
-  //   folder: "mern",
-  // });
+  const imageName = req.body.imageUrl;
+  const result = await cloudinary.uploader.upload(`${imageName}`, {
+    public_id: `${Date.now()}`,
+    resource_type: "auto",
+    folder: "mern",
+  });
 
   try {
     const doc = new PostModel({
